cb90163ef593c636c16477424fdcd1ba9f173b37
mariiaborsuk <mariaborsuk17@gmail.com>
2024-05-06 23:22:04 +0200

    levels added

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,20 +1,28 @@
-import {useState} from "react";
-function Modal({open,load, setOpen, level, setLevel, row, cell}){
-  let[cell1,setCell]=useState(cell);
-  let[row1,setRow]=useState(row);
+import {useState,useEffect} from "react";
+import {useContext} from "react";
+import gameContext from "../context/Context";
+function Modal(){
+  let{ level,setLevel,win,setWin,open,setOpen}=useContext(gameContext)
+let [fn, setFn]=useState(0)
   let className=open?"block":"hidden";
 function clickButton(){
-  setCell(cell1+1);
-  setRow(row1+1);
-  setLevel(level+1);
-  load(row1,cell1)
+
+  // setFn(fn+1);
+  // setOpen(false);
+  // setLevel(level+1);
+  // console.log("Click function was evoked",fn,"level",level,"open",open)
+  // setWin(true);
+
+
 }
+
+
   return <div className={`modal ${className} absolute w-full h-full flex flex-col justify-center z-50 items-center`}>
     <div className="rounded-md  modalSquare opacity-100 w-1/2 h-1/2 m-25 bg-purple-700 ">
      <div className="w-full flex flex-row justify-end items-end">
        <button onClick={()=>{setOpen(false)}} className="bg-blue-300 text-xl text-white px-4 mt-2 rounded-lg py-2 mr-2">X</button>
      </div>
-      <div> <button onClick={clickButton}>Next Level =></button></div>
+      <div> <button onClick={()=>{clickButton()}}>Next Level =></button></div>
 
 
     </div>
--- src/context/Context.js
+++ src/context/Context.js
@@ -3,13 +3,34 @@ import {useState} from "react";
 import {useEffect} from "react";
 let gameContext=createContext();
 function GameProvider({children}){
-  let [cellsAr, setCells]=useState([]);
-  let [idAr, setIdAr]=useState([]);
-  let[indexes, setIndexes]=useState([]);
-  let[cellsToShow, setCellsToShow]=useState([])
+  let[clickedUrl, setUrl]=useState([])
+  let [removed, setRemoved]=useState([]);
+  let [score,setScore]=useState(0);
+  let [open, setOpen]=useState(false);
+  let[win,setWin]=useState(false);
+  let [level,setLevel]=useState(1);
+  let [rowNum,setRow]=useState(2);
+  let [cellNum,setCell]=useState(3);
+
 
 
 let valueToShare={
+    clickedUrl:clickedUrl,
+  setUrl:setUrl,
+  removed:removed,
+  setRemoved:setRemoved,
+  score:score,
+  setScore:setScore,
+  open:open,
+  setOpen:setOpen,
+  win:win,
+  setWin:setWin,
+  level:level,
+  setLevel:setLevel,
+  rowNum:rowNum,
+  cellNum:cellNum,
+  setRow:setRow,
+  setCell:setCell,
 
 }
 return <gameContext.Provider value={valueToShare}>{children}</gameContext.Provider>
